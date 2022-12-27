c1134de3dd7dbc81c5939b98fe0e48fd88eaaba8
Christos Mousoulides <cm10g19@soton.ac.uk>
2022-12-27 18:33:15 +0200

    Scripts for NPC Dialogue

--- Project Folder/Assets/Scripts/DialogueSystem/NPCManager.cs
+++ Project Folder/Assets/Scripts/DialogueSystem/NPCManager.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NPCManager : MonoBehaviour
+public class NPCManager : NPCAction
 {
    private bool canBeRead = false;
    public QuestGiver questGiver;
